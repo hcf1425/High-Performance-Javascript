3ec6d89e6ee0a3bb71d17ced66d979547a615c65
huangchengfang <hcf1425@163.com>
2019-03-03 15:37:00 +0800

    保证多个脚本的加载顺序

--- 第01章-加载和执行/e09-兼容标准及IE特有的实现函数.js
+++ 第01章-加载和执行/e09-兼容标准及IE特有的实现函数.js
@@ -18,3 +18,18 @@ function loadScript(url, callback){
     script.src = null;
     document.getElementsByTagName("head")[0].appendChild(script)
 }
+
+loadScript("file1.js",function(){
+    loadScript("file2.js",function(){
+        loadScript("file3.js",function(){
+            alert("all files are loaded!")
+        })
+    })
+})
+
+/*
+    保证文件顺序加载
+    这段代码会先加载file1.js，等待file1.js加载完了之后再加载file2.js,以此类推，最后加载file3.js。
+    虽然方案可行，但是如果需要下载的文件较多，这个方案会带来一点管理上的麻烦
+    如果多个文件下载的顺序很重要，更好的做法是把他们按照正确的顺序合并成一个文件。
+*/
